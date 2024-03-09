976fab071b3625e45ca55e24ba610f26e864782f
hedi72 <heditaieb72@gmail.com>
2024-03-09 10:00:09 +0100

    iterating through

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  const allEmployees = [];
+  for (const department of Object.keys(report.allEmployees)) {
+    allEmployees.push(...report.allEmployees[department]);
+  }
+
+  return allEmployees;
+}
