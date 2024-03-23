acf06bf98437012600c622dd2f5d0e419112bcd6
hedi72 <heditaieb72@gmail.com>
2024-03-23 12:57:41 +0100

    task2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,57 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    } else if (
+      !Array.isArray(students)
+      || students.every((elm) => typeof elm !== 'string')
+    ) {
+      throw new TypeError('students must be an array of strings');
+    } else {
+      this._name = name;
+      this._length = length;
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = value;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = value;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (
+      !Array.isArray(value)
+      || value.every((elm) => typeof elm !== 'string')
+    ) {
+      throw new TypeError('students must be an array of strings');
+    } else {
+      this._students = value;
+    }
+  }
+}
