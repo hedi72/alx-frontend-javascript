a508615c73d00e68ac1022746e9cbc0f6e53eecc
hedi72 <heditaieb72@gmail.com>
2024-03-23 12:21:49 +0100

    task

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,11 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
+  const classrooms = [];
   const classRoom1 = new ClassRoom(19);
   const classRoom2 = new ClassRoom(20);
   const classRoom3 = new ClassRoom(15);
+  classrooms.push(classRoom1, classRoom2, classRoom3);
 
-  return [classRoom1, classRoom2, classRoom3];
+  return classrooms;
 }
