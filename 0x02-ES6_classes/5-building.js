21c8500920230f1ab02d59cd68fd26dfb5cc6812
hedi72 <heditaieb72@gmail.com>
2024-03-23 14:36:04 +0100

    task5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,29 @@
+export default class Building {
+  constructor(sqft) {
+    if (
+      this.constructor !== Building &&
+      typeof this.evacuationWarningMessage !== "function"
+    ) {
+      throw new Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+    }
+    if (typeof sqft !== "number") {
+      throw new TypeError();
+    } else {
+      this._sqft = sqft;
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== "number") {
+      throw new TypeError();
+    } else {
+      this._sqft = value;
+    }
+  }
+}
