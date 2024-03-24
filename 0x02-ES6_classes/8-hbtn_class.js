1c2025a4a88c3e123e88dca6184d84f2175af9cc
hedi72 <heditaieb72@gmail.com>
2024-03-24 14:16:33 +0100

    task9

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,13 +1,38 @@
-export default class HolbertonClass {
-  constructor(size, location, teacher) {
+// export default class HolbertonClass {
+//   constructor(size, location, teacher) {
+//     this._size = size;
+//     this._location = location;
+//     this._teacher = teacher;
+//   }
+
+//   [Symbol.toPrimitive](hint) {
+//     if (hint === 'number') return this._size;
+//     if (hint === 'string') return this._location;
+//     return this._teacher;
+//   }
+// }
+
+class HolbertonClass {
+  constructor(size, location) {
     this._size = size;
     this._location = location;
-    this._teacher = teacher;
   }
 
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'number') return this._size;
-    if (hint === 'string') return this._location;
-    return this._teacher;
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
   }
 }
+
+export default HolbertonClass;
